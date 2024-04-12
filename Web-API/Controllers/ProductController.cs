603144c97d430a56b0532445e4a698f1513a711c
erin8 <erin8@ERIN>
2024-04-12 16:34:05 +1000

    12/04/2024 || Created ProductController and ProductModel. To separate concepts, put details data manipulation on ProductModel instead of Product.cs. Already tested HTTP GET service with Swagger.

---
+++ Web-API/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_API.Models;
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly storeContext _dbContext;
+        public ProductController(storeContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            try
+            {
+                var productModel = new ProductModel(_dbContext);
+                var products = productModel.GetAllProducts();
+                return products == null ? NotFound() : Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct([FromRoute] int id) 
+        {
+            try
+            {
+                var productModel = new ProductModel(_dbContext);
+                var product = productModel.GetProduct(id);
+                return product == null ? NotFound() : Ok(product);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+
+}
---
+++ Web-API/Models/ProductModel.cs
@@ -0,0 +1,41 @@
+﻿namespace Web_API.Models
+{
+    public class ProductModel
+    {
+        private readonly storeContext _dbContext;
+        public ProductModel(storeContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            try
+            {
+                return _dbContext.Products.ToList();
+            }
+            catch (Exception ex)
+            {             
+                return new List<Product>();
+            }
+        }
+
+        public Product? GetProduct(int id)
+        {
+            try
+            {
+                var productItem = _dbContext.Products.Find(id);
+                if (productItem == null)
+                {
+                    return null;
+                }
+
+                return productItem;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
